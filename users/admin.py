2154106414edc176c83a0c8ccb5907bf2a71ef55
mlziade <ziade.michel@hotmail.com>
2025-04-12 22:49:35 -0300

    Add ProfileAdmin for managing user profiles in admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'last_login', 'personal_url')
+    list_display_links = ('id', 'username')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'date_joined', 'last_login')
+    search_fields = ('id', 'username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('date_joined', 'last_login')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('personal_url',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('personal_url',)}),
+    )
