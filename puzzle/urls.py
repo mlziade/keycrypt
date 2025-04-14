cea1e6a27033f9d9a0455933b4461236adbce293
mlziade <ziade.michel@hotmail.com>
2025-04-13 23:05:51 -0300

    Comment out 'My Puzzles' link in navigation for future use; update URL patterns to reflect changes

--- puzzle/urls.py
+++ puzzle/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('create/', CreatePuzzleView.as_view(), name='create_puzzle'),
     path('view/<uuid:puzzle_id>/', ViewPuzzleView.as_view(), name='view_puzzle'),
     path('solve/<uuid:puzzle_id>/', SolvePuzzleView.as_view(), name='solve_puzzle'),
-    path('my_puzzles/', MyPuzzlesView.as_view(), name='my_puzzles'),
+    # path('my_puzzles/', MyPuzzlesView.as_view(), name='my_puzzles'),
     path('test_question/<uuid:puzzle_id>/<uuid:question_id>/', test_question, name='test_question'),
 ]
