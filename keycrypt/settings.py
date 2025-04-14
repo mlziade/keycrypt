c50def054e99c313a5d5faf07ee5150625b1d831
mlziade <ziade.michel@hotmail.com>
2025-04-13 23:29:19 -0300

    Add .env file for environment variables; update settings to use SECRET_KEY and DEBUG from .env

--- keycrypt/settings.py
+++ keycrypt/settings.py
@@ -12,22 +12,24 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%r8l)&bf)-4(y0-%7bvow0!#bjdn%)zl8pq%((b7%6$totf(q%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
