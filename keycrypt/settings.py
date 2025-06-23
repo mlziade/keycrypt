bd8fe2de42caa4fec829eca4335d1fd42299e1a6
mlziade <ziade.michel@hotmail.com>
2025-06-23 16:34:46 -0300

    feat: Add .env.example for environment variable configuration and update settings.py to use new variables

--- keycrypt/settings.py
+++ keycrypt/settings.py
@@ -24,17 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [
-    'www.keycrypt.mlziade.com.br',
-    'keycrypt.mlziade.com.br',
-    '188.245.32.185',
-    '127.0.0.1'
-]
+# Get allowed hosts from environment variable, fallback to localhost for development
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 # Application definition
 
